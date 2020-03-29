821b919db77ef5c12575903189c19a92b1cab3b6
Carter Gruss <cwgruss@gmail.com>
2020-03-29 10:31:25 -0500

    feat(Express): Initaialize a basic Express server
    The instructions provided by Auth0 on their blog suggest creating a basic node application written in TypeScript, and which utilizes Express and Webpack.
    
    https://auth0.com/blog/use-typescript-to-create-a-secure-api-with-nodejs-and-express-getting-started/#Use-Environmental-Variables

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+// ----------------------------------------
+//  Express Server: 
+// ---
+//  Sunday, March 29 2020
+// ----------------------------------------
+
+
+// ########################################
+// # Required External Modules
+// ########################################
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+// Load any environmental variables from .env
+dotenv.config();
+
+// ########################################
+// # Application Variables
+// ########################################
+if (!process.env.PORT) {
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT);
+const app = express();
+
+// ########################################
+// # Application Configuration
+// ########################################
+/** Mount all external middleware functions */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// ########################################
+// # Server Activation
+// ########################################
+
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+// ########################################
+// # Webpack HMR (hot module reload)
+// ########################################
